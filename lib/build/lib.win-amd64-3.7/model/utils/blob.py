49c8de393b7ee85d8aa45bd4c6aad19fecce80d1
Brian Chap <53666335+brianchap@users.noreply.github.com>
2019-08-03 19:51:19 -0700

    Update blob.py

--- lib/build/lib.win-amd64-3.7/model/utils/blob.py
+++ lib/build/lib.win-amd64-3.7/model/utils/blob.py
@@ -1,10 +1,3 @@
-# --------------------------------------------------------
-# PyTorch Faster R-CNN
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Kevin Cao, based on code from Jianwei Yang
-# --------------------------------------------------------
-
-
 """Blob helper functions."""
 
 import numpy as np
