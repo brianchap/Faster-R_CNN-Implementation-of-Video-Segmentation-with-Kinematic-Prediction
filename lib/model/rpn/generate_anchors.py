1191d8cb477b2795e21b050b4a8cf11a27683b33
Brian Chap <53666335+brianchap@users.noreply.github.com>
2019-08-03 19:29:06 -0700

    Update generate_anchors.py

--- lib/model/rpn/generate_anchors.py
+++ lib/model/rpn/generate_anchors.py
@@ -1,10 +1,3 @@
-# --------------------------------------------------------
-# PyTorch Faster R-CNN
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Kevin Cao, based on code from Jianwei Yang
-# --------------------------------------------------------
-
-
 import numpy as np
 import pdb
 
