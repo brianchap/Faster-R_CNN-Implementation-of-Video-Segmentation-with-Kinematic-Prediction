adde73ed820a609a5743297fcb04004b862606bd
Brian Chap <53666335+brianchap@users.noreply.github.com>
2019-08-05 10:16:58 -0700

    Update initial.py

--- lib/model/utils/initial.py
+++ lib/model/utils/initial.py
@@ -3,12 +3,12 @@ import numpy as np
 import cv2
 
 # newfunction should:
-#   1. Compare values stored in z and b, as well as y and a.
+#   1. Compare values stored in y and b, as well as x and a.
 #   2. Find the two subjects of minimal distance and calculate velocity. Remove them
 #      from the tensors. Add the velocity to a tensor (magnitude and direction). Add
 #      the pascal_class into another tensor.
 #   3. Repeat step 2 until completed.
 #   4. Return two tensors, one for velocities, the other for pascal_class.
 
-def newfunction(z, y, b, a):
-    return z, y
+def newfunction(y, x, b, a):
+    return y, x
