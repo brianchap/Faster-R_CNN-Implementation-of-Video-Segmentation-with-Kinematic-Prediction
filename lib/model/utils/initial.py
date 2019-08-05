c61fc7e9ef5cc8d157074536286a752fbad55732
Brian Chap <53666335+brianchap@users.noreply.github.com>
2019-08-05 13:11:53 -0700

    Update initial.py

--- lib/model/utils/initial.py
+++ lib/model/utils/initial.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 # newfunction should:
-#   1. Compare values stored in f and i, as well as x and a and k and y.
+#   1. See the whiteboard in lab.
 #   2. Find the two subjects of minimal distance and calculate velocity and acceleration.
 #      Remove them from the tensors/arrays. Add the velocity and acceleration to four tensors
 #      (vx, vy, ax, ay). Add the pascal_class into an array.
@@ -11,4 +11,41 @@ import cv2
 #   4. Return the tensors and the array.
 
 def newfunction(f, x, i, a, k, y):
+    flipped = 0
+    oddnum = 0
+    lst = list(f)
+    lst2 = list(i)
+    lst3 = list(f)
+    lst4 = list(i)
+    f2 = 0
+    i2 = 0
+    while lst[f2] != lst2[i2]:
+       while lst[f2] < lst2[i2]:
+          f2 = f2 + 1
+       while lst[f2] > lst2[i2]:
+          i2 = i2 + 1
+    f3 = f2
+    i3 = i2
+    fcount = 0
+    icount = 0
+    while lst[f3] == lst[f3 + 1]:
+       fcount = fcount + 1
+       f3 = f3 + 1
+    while lst2[i3] == lst2[i3 + 1]:
+       icount = icount + 1
+       i3 = i3 + 1
+    if fcount > icount:
+       flipped = 1
+       lst3 = lst(i)
+       lst4 = lst(f)
+       igoogol = f2
+       f2 = i2
+       i2 = igoogol
+       if icount % 2 == 1:
+          oddnum = 1
+    else:
+       if fcount % 2 == 1:
+          oddnum = 1
+    # At this point, lst3[f2] and lst4[i2] will store the starting location at which to compare
+    # locations.
     return f, x, i, a
